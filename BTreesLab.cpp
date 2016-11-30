a49ec5b3886323c859704a38de9b29a474a7bd37
Harrison Tran <atxharrison.tran@gmail.com>
2016-11-30 11:12:07 -0600

    Add transverse and search methods

--- BTreesLab.cpp
+++ BTreesLab.cpp
@@ -73,6 +73,56 @@ BTreeNode::BTreeNode(int _min, bool _leaf)
     k = 0;
 }
 
+// Function to traverse all nodes in a subtree rooted with this node
+void BTreeNode::traverse()
+{
+    // There are n keys and n+1 children, travers through n keys
+    // and first n children
+    int i;
+    for (i = 0; i < k; i++)
+    {
+        // If this is not leaf, then before printing key[i],
+        // traverse the subtree rooted with child C[i].
+        if (leaf == false) 
+        {
+            C[i]->traverse();
+        }
+        cout << " " << keys[i];
+    }
+ 
+    // Print the subtree rooted with last child
+    if (leaf == false)
+    {
+        C[i]->traverse();
+    }
+}
+ 
+// Function to search key k in subtree rooted with this node
+BTreeNode *BTreeNode::search(int q)
+{
+    // Find the first key greater than or equal to k
+    int i = 0;
+    while (i < k && q > keys[i])
+    {
+        i++;
+    }
+ 
+    // If the found key is equal to k, return this node
+    if (keys[i] == k)
+    {
+        return this;
+    }
+ 
+    // If key is not found here and this is a leaf node
+    if (leaf == true)
+    {
+        return NULL;
+    }
+ 
+    // Go to the appropriate child
+    return C[i]->search(k);
+}
+
 int main() {
 	cout << "hi";
 	return 0;
